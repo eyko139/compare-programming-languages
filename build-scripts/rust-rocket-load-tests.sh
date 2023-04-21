959d155a34e9f1cfe9735e1a578ac339a1a16e47
Lukas Kühne <lukas.kuehne@prodyna.com>
2023-04-21 10:28:16 +0200

    versioning

--- build-scripts/rust-rocket-load-tests.sh
+++ build-scripts/rust-rocket-load-tests.sh
@@ -3,5 +3,8 @@
 #set -x
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/"
 MICROSERVICE_NAME="rust-rocket"
+MICROSERVICE_VERSION="$(cat "${SCRIPT_DIR}/../${MICROSERVICE_NAME}/.version")"
+echo VERSION: $MICROSERVICE_VERSION
 
-"${SCRIPT_DIR}/generic-load-tests.sh" --serviceName ${MICROSERVICE_NAME}
+"${SCRIPT_DIR}/generic-load-tests.sh" --serviceName ${MICROSERVICE_NAME} --serviceVersion ${MICROSERVICE_VERSION}
+
